3443559072cc4425e319abcd444e0b4082d1da3e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-15 23:34:40 +0530

    Notify Button functionality

--- first-store/src/app/products-list/products-list.component.ts
+++ first-store/src/app/products-list/products-list.component.ts
@@ -18,7 +18,7 @@ export class ProductsListComponent implements OnInit {
     window.alert('The product has been shared!');
   }
 
-  onNotify() {
+  onNotifyUser() {
     window.alert('You will be notified when the product goes on sale');
   }
   
