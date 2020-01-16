0983f451f3c78acc1d467384139461d6848f7be5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-16 23:24:52 +0530

    few changes

--- first-store/src/app/app-routing.module.ts
+++ first-store/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductsListComponent } from './products-list/products-list.component'; 
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: ProductsListComponent }
+  { path: 'home', component: ProductsListComponent },
+  { path: 'product/:productId', component: ProductDetailsComponent }
 ];
 
 @NgModule({
--- first-store/src/app/app.module.ts
+++ first-store/src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBandComponent } from './top-band/top-band.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBandComponent,
     ProductsListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
