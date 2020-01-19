ca40983e8488bb1e40bac86a5be1cf5868467736
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-19 22:30:45 +0530

    Added the Buy button functionality

--- first-store/src/app/product-details/product-details.component.ts
+++ first-store/src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { productslist } from '../products-list/productslist';
+import { CartService } from '../services/cart.service';
 
 @Component({
 	selector: 'app-product-details',
@@ -12,13 +13,19 @@ export class ProductDetailsComponent implements OnInit {
 
 	product;
 
-	constructor(private route: ActivatedRoute) {
+	constructor(private route: ActivatedRoute, private cartService: CartService) {
 		this.route.paramMap.subscribe(params => {
 			this.product = productslist[parseInt(params.get('productId'))];
 		});
 	}
 
 	ngOnInit() {
+	}
+
+	addProductToCart(product) {
+		this.cartService.addProductToCart(product);
+
+		window.alert('Selected product has been added to the cart!');
 	}
 
 }
