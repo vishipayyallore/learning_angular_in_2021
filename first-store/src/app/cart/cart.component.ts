b5cac90b3637a67a1382608d73e6ebd8a370631d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-20 20:43:46 +0530

    Check out form

--- first-store/src/app/cart/cart.component.ts
+++ first-store/src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 import { CartService } from '../services/cart.service';
 
@@ -8,14 +9,26 @@ import { CartService } from '../services/cart.service';
 	styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
 	selectedProducts = [];
+	checkoutForm;
 
-	constructor(private cartService: CartService) {
+	constructor(private cartService: CartService, private formBuilder: FormBuilder) {
+		this.checkoutForm = this.formBuilder.group({
+			name: '',
+			address: ''
+		});
 	}
 
 	ngOnInit() {
 		this.selectedProducts = this.cartService.getProductsFromCart();
+	}
+
+	onSubmit(customerData) {
+		// Process Checkout Data here
+		console.warn(`Your order has been submitted ${customerData}`);
+
+		this.selectedProducts = this.cartService.clearSelectedProducts();
+		this.checkoutForm.reset();
 	}
 
 }
