7709eb3d8b4f91063c96dfab4954e4f37ab54105
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-02-02 12:49:49 +0530

    Update product-alerts.component.spec.ts

--- first-store/src/app/product-alerts/product-alerts.component.spec.ts
+++ first-store/src/app/product-alerts/product-alerts.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductAlertsComponent } from './product-alerts.component';
+import { ProductDto } from '../interfaces/ProductDto';
+import { ProductDetailsComponent } from '../product-details/product-details.component';
 
 describe('ProductAlertsComponent', () => {
   let component: ProductAlertsComponent;
   let fixture: ComponentFixture<ProductAlertsComponent>;
+  let product: ProductDto;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,10 +19,13 @@ describe('ProductAlertsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductAlertsComponent);
     component = fixture.componentInstance;
+
+    product = {id: 1, price:123.34, name: 'Dummy Product', description: 'Dummy Description'};
+    component.product = product;
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
