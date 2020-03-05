e5d300dcf6d7afb85c73f6abf44fa3bbdb722549
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-03-05 23:48:05 +0530

    Initial Version

--- sample-products/src/app/app-routing.module.ts
+++ sample-products/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ProductsListComponent } from './products-list/products-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'home', component: LandingPageComponent },
+	{ path: 'products', component: ProductsListComponent },
+	{ path: '', redirectTo: 'home', pathMatch: 'full' },
+	{ path: '**', redirectTo: 'home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sample-products/src/app/app.module.ts
+++ sample-products/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBandComponent } from './top-band/top-band.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ModifyProductComponent } from './modify-product/modify-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBandComponent
+    TopBandComponent,
+    ProductsListComponent,
+    LandingPageComponent,
+    AddProductComponent,
+    ModifyProductComponent
   ],
   imports: [
     BrowserModule,
