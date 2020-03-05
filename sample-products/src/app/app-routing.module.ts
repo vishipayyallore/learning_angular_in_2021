e815e245d145f319affc13e1520f23674121c788
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-03-06 00:02:29 +0530

    ,,

--- sample-products/src/app/app-routing.module.ts
+++ sample-products/src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ModifyProductComponent } from './modify-product/modify-product.component';
 
 const routes: Routes = [
 	{ path: 'home', component: LandingPageComponent },
 	{ path: 'products', component: ProductsListComponent },
+	{ path: 'addproduct', component: AddProductComponent },
+	{ path: 'modifyproduct', component: ModifyProductComponent },
 	{ path: '', redirectTo: 'home', pathMatch: 'full' },
 	{ path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
