c048795c750c93f6c3f38dbfd8a64dae27bf12dd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-03-14 10:22:57 +0530

    Few Changes

--- simple-display/src/app/app.component.ts
+++ simple-display/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-display';
+    title = 'simple-display';
+
+    readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+    posts: any[] = [];
+    constructor(private http: HttpClient) {
+    }
+
+    getPosts() {
+        this.http.get(this.ROOT_URL + '/posts/1').subscribe((post) => {
+            this.posts = [post];
+        });
+    }
+
 }
