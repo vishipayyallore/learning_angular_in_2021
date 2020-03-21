da1ffad691eea406bdc2d3b3b7800976c2b907a9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-03-21 23:54:27 +0530

    Few Changes

--- sample-products/src/app/modify-product/modify-product.component.ts
+++ sample-products/src/app/modify-product/modify-product.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ProductDto } from '../interfaces/ProductDto';
+import { ProductsService } from '../services/products.service';
 
 @Component({
-  selector: 'app-modify-product',
-  templateUrl: './modify-product.component.html',
-  styleUrls: ['./modify-product.component.css']
+    selector: 'app-modify-product',
+    templateUrl: './modify-product.component.html',
+    styleUrls: ['./modify-product.component.css']
 })
 export class ModifyProductComponent implements OnInit {
 
-  constructor() { }
+    product: any;
 
-  ngOnInit(): void {
-  }
+    constructor(private route: ActivatedRoute, private productsService: ProductsService) {
+        this.productsService.GetProductById(1)
+                    .subscribe((data: any) => {
+                        this.product = {id: 1, name: 'Shiva'};
+                    });
+
+        /*
+        this.route.paramMap.subscribe(params => {
+            this.productsService.GetProductById(parseInt(params.get('productId')))
+                .subscribe((data: ProductDto) => {
+                    this.product = data;
+                });
+            console.log(`${this.product}`);
+        });
+        */
+    }
+
+    ngOnInit(): void {
+        console.log(`${this.product}`);
+    }
 
 }
--- sample-products/src/app/services/products.service.ts
+++ sample-products/src/app/services/products.service.ts
@@ -30,13 +30,23 @@ export class ProductsService {
             )
     }
 
-    // GET
+    // GET All Products
     GetProducts(): Observable<ProductDto[]> {
         return this.httpClient.get<ProductDto[]>(`${baseUrl}/products`)
             .pipe(
                 retry(1),
                 catchError(this.errorHandler)
             );
+    }
+
+    // GET
+    GetProductById(id: number): Observable<ProductDto> {
+        console.log(`Get Product request received for ${id}`);
+        return this.httpClient.get<ProductDto>(`${baseUrl}/products/${id}`)
+            .pipe(
+                retry(1),
+                catchError(this.errorHandler)
+            )
     }
 
     // Error handling
