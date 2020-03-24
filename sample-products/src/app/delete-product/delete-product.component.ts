594720a13b668c630f5f7752b238f6b85da50d8c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-03-24 22:31:46 +0530

    Working on Delete Screen

--- sample-products/src/app/delete-product/delete-product.component.ts
+++ sample-products/src/app/delete-product/delete-product.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ProductDto } from '../interfaces/ProductDto';
+import { ProductsService } from '../services/products.service';
 
 @Component({
-  selector: 'app-delete-product',
-  templateUrl: './delete-product.component.html',
-  styleUrls: ['./delete-product.component.css']
+    selector: 'app-delete-product',
+    templateUrl: './delete-product.component.html',
+    styleUrls: ['./delete-product.component.css']
 })
 export class DeleteProductComponent implements OnInit {
 
-  constructor() { }
+    product: ProductDto;
 
-  ngOnInit(): void {
-  }
+    constructor(private route: ActivatedRoute, private productsService: ProductsService) {
+    }
+
+    ngOnInit(): void {
+        this.route.paramMap.subscribe(params => {
+            this.productsService.GetProductById(parseInt(params.get('productId')))
+                .subscribe((data: ProductDto) => {
+                    this.product = data;
+                    console.log(`${this.product.name}`);
+                });
+        });
+    }
 
 }
