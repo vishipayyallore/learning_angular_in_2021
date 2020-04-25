d9752be900abb3da4bd233b48ae6287270e55e4e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-04-25 22:38:29 +0530

    ..

---
+++ typescript-demos/day1v1/src/scripts/person.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function Person(name, gender) {
+
+    // Properties
+    this.name = name;
+    this.gender = gender;
+
+    // Methods
+    this.getPersonDetails = function() {
+        return `Name: ${this.name} | Gender: ${this.gender}`;
+    };
+}
+
+Person.prototype.getPersonDetailsV1 = function() {
+    return `Name: ${this.name} | Gender: ${this.gender}`;
+};
+
+/*
+    Description: To Avoid the pollution of Global Namespace
+function getPerson() {
+    return `Name: ${this.name} | Gender: ${this.gender}`;
+}
+*/
---
+++ typescript-demos/day1v1/src/scripts/student.js
@@ -0,0 +1,9 @@
+var student = {
+    name: "Manish Singh",
+    standard: "12",
+
+    getStudentDetails: function () {
+        return `Name: ${this.name} | ${this.standard} Standard.`;
+    }
+}
+
