a49944849db0c698809a7b209b768242028871f1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-05-03 19:07:27 +0530

    Simple Application

---
+++ typescript-demos/day2/src/greeter.ts
@@ -0,0 +1,27 @@
+"use strict";
+
+interface Person {
+    firstName: string;
+
+    lastName: string;
+}
+
+class Student {
+    fullName: string = "";
+
+    constructor(public firstName: string, public lastName: string) {
+        this.fullName = `${this.firstName} ${this.lastName}`;
+    }
+}
+
+function greeter(person: Person) {
+    return `Hello ${person.firstName} ${person.lastName}`;
+}
+
+var user = new Student("Shiva", "Sai");
+
+function setTextBoxMessage() {
+    let textBoxMessage = document.getElementById("textBoxMessage") as HTMLInputElement;
+    textBoxMessage.value = user.fullName;
+}
+
