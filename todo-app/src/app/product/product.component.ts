bb44fb2b67921caaa994b9dcd8e15ca96b58e108
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-05-19 23:46:32 +0530

    Few Changes

--- todo-app/src/app/product/product.component.ts
+++ todo-app/src/app/product/product.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+    productName: String = "All In One PC";
+    valueForConstructor: String = '';
+    valueForButton: String = '';
+    isButtonDisabled = true;
 
-  constructor() { }
+    constructor() { 
+        setTimeout(() => {
+            this.valueForConstructor = "All In One PC Modified";
+            this.isButtonDisabled = false;
+        }, 3000);
 
-  ngOnInit(): void {
-  }
+    }
+
+    ngOnInit(): void {
+    }
 
 }
