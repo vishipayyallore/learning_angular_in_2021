eb5e916120f5ebc9625a8059aca214b6db3bfd94
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-06-03 23:58:43 +0530

    ..

--- typescript-demos/day6/src/index.js
+++ typescript-demos/day6/src/index.js
@@ -1,1 +1,20 @@
-var one = new Promise(function (resolve, reject) { });
+function asyncFun() {
+    var promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log("Console from promise function");
+            resolve();
+        }, 3000);
+    });
+    return promise;
+}
+/*
+
+a = () =>{ return new Promise((resolve,reject)=> {
+    setTimeout(()=>{
+        console.log("a")
+        resolve('inside a()');
+
+    },1000);   // 1 sec delay
+})
+
+*/ 
--- typescript-demos/day6/src/index.ts
+++ typescript-demos/day6/src/index.ts
@@ -1,3 +1,23 @@
 
 
-const one = new Promise<string>((resolve, reject) => {});
+function asyncFun() {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Console from promise function");
+            resolve();
+        }, 3000);
+    });
+    return promise;
+}
+
+/*
+
+a = () =>{ return new Promise((resolve,reject)=> {
+    setTimeout(()=>{
+        console.log("a")
+        resolve('inside a()');
+
+    },1000);   // 1 sec delay
+})
+
+*/
