76f1e0d7f74a9aa36d380efe158237277eb80721
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-04-21 16:52:04 +0530

    Today's work

--- localstorage-bookstore/src/app/components/books/books-list/books-list.component.ts
+++ localstorage-bookstore/src/app/components/books/books-list/books-list.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
 
-import { LocalstorageService } from '../../services/localstorage.service';
 import { Ibook } from '../../interfaces/ibook';
+import { BooksService } from '../../services/books.service';
 
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
   styleUrls: ['./books-list.component.css']
 })
-export class BooksListComponent implements OnInit {
+export class BooksListComponent implements OnInit, OnDestroy {
 
   booksList: Ibook[];
   imageWidth = 50;
   imageMargin = 1;
+  booksServiceSubscription: Subscription;
 
   // Dependency Injection (Construction Injection)
-  constructor(private localStorageService: LocalstorageService) { }
+  constructor(private booksService: BooksService) {
+  }
 
   ngOnInit(): void {
+    this.booksServiceSubscription = this.booksService.getAllBooks()
+      .subscribe((data: Ibook[]) => {
+        this.booksList = data;
+        console.log(this.booksList);
+      });
+  }
 
-    this.booksList = JSON.parse(this.localStorageService.getItem('books'));
-
+  ngOnDestroy() {
+    this.booksServiceSubscription.unsubscribe()
   }
 
 }
--- localstorage-bookstore/src/app/components/services/books.service.ts
+++ localstorage-bookstore/src/app/components/services/books.service.ts
@@ -1,15 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Subscriber } from 'rxjs/internal/Subscriber';
+
 import { LocalstorageService } from './localstorage.service';
+import { Ibook } from '../interfaces/ibook';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  constructor(private localStorageService: LocalstorageService) { 
+  booksList: Ibook[];
+
+  constructor(private localStorageService: LocalstorageService) {
 
   }
-  
-  // getAllBooks()
+
+  getAllBooks(): Observable<Ibook[]> {
+    this.booksList = JSON.parse(this.localStorageService.getItem('books'));
+
+    return new Observable<Ibook[]>(
+      (subscriber: Subscriber<Ibook[]>) => {
+        setTimeout(() => {
+          subscriber.next(this.booksList)
+          subscriber.complete();
+        }, 500);
+      });
+
+  }
 
 }
