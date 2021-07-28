c2ac67d3b9ede5902ffc5ad8e4f71f574951714b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-28 12:17:19 +0530

    Today's work

--- ng-projecttemplate/src/app/app-routing.module.ts
+++ ng-projecttemplate/src/app/app-routing.module.ts
@@ -7,11 +7,15 @@ import { ContactusComponent } from './components/home/contactus/contactus.compon
 import { PageNotfoundComponent } from './components/shared/page-notfound/page-notfound.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'aboutus', component: AboutusComponent },
-  { path: 'contactus', component: ContactusComponent },
+  // { path: 'dashboard', component: DashboardComponent },
+  // { path: 'aboutus', component: AboutusComponent },
+  // { path: 'contactus', component: ContactusComponent },
+  {
+    path: 'home',
+    loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule),
+  },
   { path: 'pagenotfound', component: PageNotfoundComponent },
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: 'home/dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'pagenotfound', pathMatch: 'full' }
 ];
 
--- ng-projecttemplate/src/app/app.module.ts
+++ ng-projecttemplate/src/app/app.module.ts
@@ -3,26 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './components/home/dashboard/dashboard.component';
+// import { DashboardComponent } from './components/home/dashboard/dashboard.component';
 import { TopNavbarComponent } from './components/shared/top-navbar/top-navbar.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { PageNotfoundComponent } from './components/shared/page-notfound/page-notfound.component';
-import { AboutusComponent } from './components/home/aboutus/aboutus.component';
-import { ContactusComponent } from './components/home/contactus/contactus.component';
+// import { AboutusComponent } from './components/home/aboutus/aboutus.component';
+// import { ContactusComponent } from './components/home/contactus/contactus.component';
+import { HomeModule } from './components/home/home.module';
+import { HomeRoutingModule } from './components/home/home-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
+    // DashboardComponent,
     TopNavbarComponent,
     FooterComponent,
     PageNotfoundComponent,
-    AboutusComponent,
-    ContactusComponent
+    // AboutusComponent,
+    // ContactusComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // HomeModule,
+    HomeRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
