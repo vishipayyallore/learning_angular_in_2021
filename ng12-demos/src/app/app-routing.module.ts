a76c4dac2f8f4911e69f0142a21edb89037decbb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-11 17:21:16 +0530

    Update app-routing.module.ts

--- ng12-demos/src/app/app-routing.module.ts
+++ ng12-demos/src/app/app-routing.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { ServerComponent } from './components/server/server.component';
+
 const routes: Routes = [
 	{
 		path: 'home',
 		loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule),
 	},
-	{ 
-		path: 'pagenotfound', 
+	{
+		path: 'pagenotfound',
 		loadChildren: () => import('./components/shared/shared.module').then(m => m.SharedModule),
 	},
+	{ path: 'servers', component: ServerComponent },
 	{ path: '', redirectTo: 'home/dashboard', pathMatch: 'full' },
 	{ path: '**', redirectTo: 'pagenotfound', pathMatch: 'full' }
 ];
