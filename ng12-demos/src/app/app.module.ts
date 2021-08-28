b7ec0d2d040d34dcbd0e19a786d09772eaf0562c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-08-28 10:26:36 +0530

    ..

--- ng12-demos/src/app/app.module.ts
+++ ng12-demos/src/app/app.module.ts
@@ -4,25 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './components/shared/shared.module';
-import { SelectorDemoComponent } from './components/demos/selector-demo/selector-demo.component';
-// import { DatabingingDemoComponent } from './components/demos/databinging-demo/databinging-demo.component';
-import { DemosLandingComponent } from './components/demos/demos-landing/demos-landing.component';
-// import { ListProductsComponent } from './components/products/list-products/list-products.component';
-// import { ProductsModule } from './components/products/products.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SelectorDemoComponent,
-    // DatabingingDemoComponent,
-    DemosLandingComponent,
-    // ListProductsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    // ProductsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ng12-demos/src/app/components/demos/demos.module.ts
+++ ng12-demos/src/app/components/demos/demos.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DemosRoutingModule } from './demos-routing.module';
 
-
+import { DemosLandingComponent } from './demos-landing/demos-landing.component';
+import { SelectorDemoComponent } from './selector-demo/selector-demo.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DemosLandingComponent,
+    SelectorDemoComponent
+  ],
   imports: [
     CommonModule,
     DemosRoutingModule
