de2edeec6250d860eede8fadb6d82845e286dbaf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-09-05 15:38:28 +0530

    Today's work

--- ng12-demos/src/app/components/products/list-products/list-products.component.ts
+++ ng12-demos/src/app/components/products/list-products/list-products.component.ts
@@ -1,22 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { IProductDto } from '@products/interfaces/iproduct.dto';
 import { ProductsService } from '@products/services/products.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sv-list-products',
   templateUrl: './list-products.component.html',
   styleUrls: ['./list-products.component.css']
 })
-export class ListProductsComponent implements OnInit {
+export class ListProductsComponent implements OnInit, OnDestroy {
 
   pageTitle = 'Product List';
   imageWidth = 40;
   imageMargin = 1;
   showImage = false;
+  showSpinner = false;
+
   errorMessage: string = '';
-  listFilter = 'cart';
+
+  subscription!: Subscription;
+
+  allProducts: IProductDto[] = [];
   products: IProductDto[] = [];
+
+  private _listFilter = '';
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.products = this.performFilter(value);
+  }
 
   constructor(private productsService: ProductsService) {
   }
@@ -25,17 +42,41 @@ export class ListProductsComponent implements OnInit {
     this.retrieveAllProducts();
   }
 
-  retrieveAllProducts() {
+  ngOnDestroy(): void {
+    console.log('Destroy !!');
+    this.subscription.unsubscribe();
+  }
 
-    this.productsService.getAllProducts()
-      .subscribe((data: IProductDto[]) => {
-        this.products = data;
-        console.log(this.products);
+  retrieveAllProducts() {
+    this.showSpinner = true;
+    this.subscription = this.productsService.getAllProducts()
+      .subscribe({
+        next: (data: IProductDto[]) => {
+          this.allProducts = this.products = data;
+        },
+        error: (err) => {
+          this.errorMessage = err;
+          this.showSpinner = false;
+        },
+        complete: () => {
+          this.showSpinner = false;
+        }
       });
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+
+  performFilter(filterBy: string): IProductDto[] {
+
+    // filterBy = filterBy.toLocaleLowerCase();
+    return this.allProducts.filter((product: IProductDto) =>
+      product.productName.toLocaleLowerCase().includes(filterBy.toLocaleLowerCase()));
+  }
+
+  get hasProducts(): boolean {
+    return this.products.length > 0;
   }
 
 }
