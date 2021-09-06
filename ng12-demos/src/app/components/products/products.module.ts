d962af7e22b605719223293506c405e785168352
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-09-06 11:16:15 +0530

    Today's work

--- ng12-demos/src/app/components/products/products.module.ts
+++ ng12-demos/src/app/components/products/products.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { ListProductsComponent } from './list-products/list-products.component';
-import { ProductsRoutingModule } from './products-routing.module';
 import { ViewProductComponent } from './view-product/view-product.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { EditProductComponent } from './edit-product/edit-product.component';
 import { DeleteProductComponent } from './delete-product/delete-product.component';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ConvertToSpacesPipe } from '@app/core/products/pipes/converttospaces.pipe';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,8 @@ import { DeleteProductComponent } from './delete-product/delete-product.componen
     ViewProductComponent,
     AddProductComponent,
     EditProductComponent,
-    DeleteProductComponent
+    DeleteProductComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
     CommonModule,
