4a5970a402988c9ba29327ebe9a54bba7b8d38e7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-09-08 23:31:11 +0530

    Update list-products.component.ts

--- ng12-demos/src/app/components/products/list-products/list-products.component.ts
+++ ng12-demos/src/app/components/products/list-products/list-products.component.ts
@@ -5,78 +5,80 @@ import { ProductsService } from '@products/services/products.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'sv-list-products',
-  templateUrl: './list-products.component.html',
-  styleUrls: ['./list-products.component.css']
+    selector: 'sv-list-products',
+    templateUrl: './list-products.component.html',
+    styleUrls: ['./list-products.component.css']
 })
 export class ListProductsComponent implements OnInit, OnDestroy {
 
-  pageTitle = 'Product List';
-  imageWidth = 40;
-  imageMargin = 1;
-  showImage = false;
-  showSpinner = false;
+    pageTitle = 'Product List';
+    imageWidth = 40;
+    imageMargin = 1;
+    showImage = false;
+    showSpinner = false;
 
-  errorMessage: string = '';
+    errorMessage: string = '';
 
-  subscription!: Subscription;
+    subscription!: Subscription;
 
-  allProducts: IProductDto[] = [];
-  products: IProductDto[] = [];
+    allProducts: IProductDto[] = [];
+    products: IProductDto[] = [];
 
-  private _listFilter = '';
+    private _listFilter = '';
 
-  get listFilter(): string {
-    return this._listFilter;
-  }
+    get listFilter(): string {
+        return this._listFilter;
+    }
 
-  set listFilter(value: string) {
-    this._listFilter = value;
-    this.products = this.performFilter(value);
-  }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.products = this.performFilter(value);
+    }
 
-  constructor(private productsService: ProductsService) {
-  }
+    constructor(private productsService: ProductsService) {
+    }
 
-  ngOnInit(): void {
-    this.retrieveAllProducts();
-  }
+    ngOnInit(): void {
+        this.retrieveAllProducts();
+    }
 
-  ngOnDestroy(): void {
-    console.log('Destroy !!');
-    this.subscription.unsubscribe();
-  }
+    ngOnDestroy(): void {
+        console.log('Destroy !!');
+        this.subscription.unsubscribe();
+    }
 
-  retrieveAllProducts() {
-    this.showSpinner = true;
-    this.subscription = this.productsService.getAllProducts()
-      .subscribe({
-        next: (data: IProductDto[]) => {
-          this.allProducts = this.products = data;
-        },
-        error: (err) => {
-          this.errorMessage = err;
-          this.showSpinner = false;
-        },
-        complete: () => {
-          this.showSpinner = false;
-        }
-      });
-  }
+    retrieveAllProducts() {
+        this.showSpinner = true;
+        this.subscription = this.productsService.getAllProducts()
+            .subscribe({
+                next: (data: IProductDto[]) => {
+                    this.allProducts = this.products = data;
+                },
+                error: (err) => {
+                    this.errorMessage = err;
+                    this.showSpinner = false;
+                },
+                complete: () => {
+                    this.showSpinner = false;
+                }
+            });
+    }
 
-  toggleImage(): void {
-    this.showImage = !this.showImage;
-  }
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
 
-  performFilter(filterBy: string): IProductDto[] {
+    performFilter(filterBy: string): IProductDto[] {
 
-    // filterBy = filterBy.toLocaleLowerCase();
-    return this.allProducts.filter((product: IProductDto) =>
-      product.productName.toLocaleLowerCase().includes(filterBy.toLocaleLowerCase()));
-  }
+        return this.allProducts.filter((product: IProductDto) =>
+            product
+                .productName
+                .toLocaleLowerCase()
+                .includes(filterBy.toLocaleLowerCase()));
+    }
 
-  get hasProducts(): boolean {
-    return this.products.length > 0;
-  }
+    get hasProducts(): boolean {
+        return this.products.length > 0;
+    }
 
 }
